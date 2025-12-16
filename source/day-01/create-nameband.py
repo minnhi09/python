0c1ecb9f9bfdaefa5cd642db50205cc6b21ed688
mnhy <lanhdtm264@gmail.com>
2025-12-16 18:51:29 +0700

    day 1

---
+++ source/day-01/create-nameband.py
@@ -0,0 +1,4 @@
+print("welcome to the Band Name Generator")
+cityname = input("what's the name of the city you grew up in? \n")
+petname = input("what's your pet name? \n")
+print("your band name could be "+cityname+" "+petname)
